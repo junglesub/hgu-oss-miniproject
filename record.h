cd916f58ca8dcd7dc0eb8b7aa9e8f877299cdfc6
Jungsub Ryoo <junglesubmarine@gmail.com>
2021-05-05 14:30:42 +0900

    포인터 배열로 변경하였습니다. #7

--- record.h
+++ record.h
@@ -2,31 +2,31 @@
 #define __RECORDS_H__
 
 typedef struct {
-	char date[20];
-	int management_type;
-	int price;
-	char paymentMethod[20];
+  char date[20];
+  int management_type;
+  int price;
+  char paymentMethod;
 } Record;
 
 // CRUD
-int createRecord(Record *r);
-void readRecord(Record *r);
-int updateRecord(Record *r); 
-void deleteRecord(Record r[], int count, int delIndex);
+int createRecord(Record *r[]);
+void readRecord(Record *r[]);
+int updateRecord(Record *r[]);
+void deleteRecord(Record *r[], int count, int delIndex);
 
 // 검색 및 출력
-void searchBreakdown(Record *r, int management_type); // 항목별 내역 확인
-void searchPrice(Record *r, int management_type); // 금액별 내역 확인
-void printMonthRecords(Record r[], int count, char* yearmonth);
-void printPaymentMethod(Record r[], int count, char paymentmethod);
+void searchBreakdown(Record *r[], int management_type);  // 항목별 내역 확인
+void searchPrice(Record *r[], int management_type);  // 금액별 내역 확인
+void printMonthRecords(Record *r[], int count, char *yearmonth);
+void printPaymentMethod(Record *r[], int count, char paymentmethod);
 
 // 불러오기 및 저장
-void save(Record r[], int count, char* filename);
-void load(Record r[], int* count, char* filename);
+void save(Record *r[], int count, char *filename);
+void load(Record *r[], int *count, char *filename);
 
 // 기타
 int selectMenu();
-void listRecord(Record *r, int record_num);
-int selectDataNo(Record *r, int record_num);
+void listRecord(Record *r[], int record_num);
+int selectDataNo(Record *r[], int record_num);
 
 #endif
