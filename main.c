38ab03cca0084c3194b7a82aad9d8d6cc1ebd5ab
Jungsub Ryoo <junglesubmarine@gmail.com>
2021-05-07 16:25:18 +0900

    메뉴 일부분 구현 완료. #35 @GwonYuri 도와줌

--- main.c
+++ main.c
@@ -4,13 +4,29 @@
 
 int main(void) {
   int sel;
+  Record *r[100];
+  int count = 0;
 
   while (1) {
     sel = selectMenu();
-
+    int no;
     switch (sel) {
       case 0:
         return 1;
+      case 1:
+        count += createRecord(r, count);
+        break;
+      case 2:
+        listRecord(r, count);
+        break;
+      case 3:
+        no = selectDataNo(r, count);
+        if (no == 0) break;
+        readRecord(r, count, no - 1);
+        break;
+      case 4:
+        // Delete
+        break;
       default:
         printf("잘못된 선택!\n");
     }
